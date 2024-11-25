8d5313de9f37efebde621e4c1671928fff8ce5a0
RoarlisF/KP31 <zhixinfang@uvic.ca>
2024-11-25 14:23:11 -0800

    Reeeee merge

--- scripts/HNSPlayer.cs
+++ scripts/HNSPlayer.cs
@@ -749,7 +749,7 @@ public partial class PlayerCorpse : Component
 			PlayerAnimator.SpineInstance.SetStateMachine(sm, Entity);
 
 			PlayerAnimator.SetCrewchsia(ColorIndex);
-			PlayerAnimator.SpineInstance.SetSkeleton(Assets.GetAsset<SpineSkeletonAsset>("hns_rig"));
+			PlayerAnimator.SpineInstance.SetSkeleton(Assets.GetAsset<SpineSkeletonAsset>("animations/player/merged_player.spine"));
 			foreach (var skin in PlayerSkins)
 			{
 				PlayerAnimator.SpineInstance.EnableSkin(skin);
